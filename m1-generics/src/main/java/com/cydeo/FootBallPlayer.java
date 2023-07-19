bde56ba97dc2173661480ff952aae109fe72f62d
Gulbahar Recep <gulbahar.rajap@gmail.com>
2023-07-19 22:33:58 +0300

    player class and its child classes created

---
+++ m1-generics/src/main/java/com/cydeo/FootBallPlayer.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+public class FootBallPlayer extends Player{
+
+
+    public FootBallPlayer(String name) {
+        super(name);
+    }
+}
---
+++ m1-generics/src/main/java/com/cydeo/SoccerPlayer.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+public class SoccerPlayer extends Player{
+
+
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+
+
+
+}
