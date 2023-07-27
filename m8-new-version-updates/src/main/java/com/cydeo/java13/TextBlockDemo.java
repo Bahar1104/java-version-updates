9ff1f8e87af488758839e34ae8560b3858586513
Gulbahar Recep <gulbahar.rajap@gmail.com>
2023-07-27 19:21:19 +0300

    java 13 updates

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java13/TextBlockDemo.java
@@ -0,0 +1,20 @@
+package com.cydeo.java13;
+
+public class TextBlockDemo {
+    public static void main(String[] args) {
+
+        var address="7925 Jones Branch Drive \n "+
+                "Mclean, va 22102";
+        System.out.println(address);
+
+
+
+        var address2="""
+                7925 Jones Branch Drive
+                Mclean, va 22102
+                """;
+        System.out.println(address2);
+        
+        
+    }
+}
