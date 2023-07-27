afb4d1a256f06aa7fa26a3ab623d37cea97a77af
Gulbahar Recep <gulbahar.rajap@gmail.com>
2023-07-27 21:21:34 +0300

    java 14 updates

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,16 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) {
+    public User {
+    }
+
+    // private final
+    // all argument constructor
+    //getter
+    //to string
+    //equals
+    //hash code
+
+
+
+}
